bc3f926396bc51a4d775e5a256cabb5b07ac07b7
Code Magic <classic@Dropcodes-Mac.local>
2020-10-17 22:06:58 +0100

    added tailwindcss

---
+++ src/components/Layouts/NavbarComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="w-full bg-purple-100 p-6">
+      <p>just a test</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+  name: 'NavbarComponent',
+
+  data () {
+    return {
+
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
